e6659e7b0ba9667e0af31a66e38a292cbae03500
Isaac Novaes <isaac.novaes@hotmail.com>
2022-04-01 23:06:00 +0200

    Add Routes and hidden horizontal overflow for animation-wise purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.module.scss";
+import { Route, Routes } from "react-router-dom";
+import styles from "./App.module.scss";
+import Page from "./pageComponent/Page";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className={styles.App}>
+			<Routes>
+				<Route path="/" element={<Page type="Home"></Page>} />
+				<Route path="/destination" element={<Page type="Destination"></Page>} />
+				<Route path="/crew" element={<Page type="Crew"></Page>} />
+				<Route path="/technology" element={<Page type="Technology"></Page>} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
