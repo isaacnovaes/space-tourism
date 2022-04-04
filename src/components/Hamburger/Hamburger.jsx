ec025eea52f976d2b2747e3f1f25d3354a976216
Isaac Novaes <isaac.novaes@hotmail.com>
2022-04-04 22:27:15 +0200

    Update Hamburger component. Bring the onclick event to the svg element

--- src/components/Hamburger/Hamburger.jsx
+++ src/components/Hamburger/Hamburger.jsx
@@ -3,8 +3,8 @@ import styles from "./Hamburger.module.scss";
 const Hamburger = ({ type = "normal", toggle }) => {
 	const hamburger =
 		type === "close" ? (
-			<div className={`${styles.Hamburger} ${styles.Close}`} onClick={toggle}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="21">
+			<div className={`${styles.Hamburger} ${styles.Close}`} >
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" onClick={toggle}>
 					<g fill="#D0D6F9" fillRule="evenodd">
 						<path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
 						<path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
@@ -12,8 +12,8 @@ const Hamburger = ({ type = "normal", toggle }) => {
 				</svg>
 			</div>
 		) : (
-			<div className={styles.Hamburger} onClick={toggle}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
+			<div className={styles.Hamburger} >
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="21" onClick={toggle}>
 					<g fill="#D0D6F9" fillRule="evenodd">
 						<path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
 					</g>
