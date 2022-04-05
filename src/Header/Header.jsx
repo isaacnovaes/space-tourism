223e922f9e2a32ad038e14c8eeab99b69b281b6e
Isaac Novaes <isaac.novaes@hotmail.com>
2022-04-05 22:51:57 +0200

    Factorize Header component

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,29 +1,23 @@
+import { useState } from "react";
 import styles from "./Header.module.scss";
+import Logo from "../components/Logo/Logo";
+import Hamburger from "../components/Hamburger/Hamburger";
+import MobileNav from "../components/MobileNav/MobileNav";
 
 const Header = () => {
+	const [showMobileNav, setShowMobileNav] = useState(false);
+	const toggleMobileNavHandler = () => setShowMobileNav(!showMobileNav);
+
 	return (
-		<header className={styles.Header}>
-			<div className={styles.HeaderMobile}>
-				<div className={styles.Logo}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
-						<g fill="none" fill-rule="evenodd">
-							<circle cx="24" cy="24" r="24" fill="#FFF" />
-							<path
-								fill="#0B0D17"
-								d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
-							/>
-						</g>
-					</svg>
+		<>
+			<header className={styles.Header}>
+				<div className={styles.HeaderMobile}>
+					<Logo />
+					{!showMobileNav && <Hamburger toggle={toggleMobileNavHandler} />}
 				</div>
-				<div className={styles.Hamburger}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
-						<g fill="#D0D6F9" fill-rule="evenodd">
-							<path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
-						</g>
-					</svg>
-				</div>
-			</div>
-		</header>
+			</header>
+			<MobileNav isVisible={showMobileNav} toggle={toggleMobileNavHandler} />
+		</>
 	);
 };
 
