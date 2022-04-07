ccacf6c6876658187cc8d40e172607d211d39777
Isaac Novaes <isaac.novaes@hotmail.com>
2022-04-07 12:12:19 +0200

    Finish header and the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import styles from "./App.module.scss";
 import Page from "./pageComponent/Page";
+import Home from "./pages/Home/Home";
 
 function App() {
 	return (
 		<div className={styles.App}>
 			<Routes>
-				<Route path="/" element={<Page type="Home"></Page>} />
+				<Route
+					path="/"
+					element={
+						<Page type="Home">
+							<Home />
+						</Page>
+					}
+				/>
 				<Route path="/destination" element={<Page type="Destination"></Page>} />
 				<Route path="/crew" element={<Page type="Crew"></Page>} />
 				<Route path="/technology" element={<Page type="Technology"></Page>} />
