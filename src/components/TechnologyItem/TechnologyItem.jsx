73358c5378a0e1616c22235407008c737321a9d9
Isaac Novaes <isaac.novaes@hotmail.com>
2022-04-11 10:59:22 +0200

    Finish mobile version of the Technology component

--- src/components/TechnologyItem/TechnologyItem.jsx
+++ src/components/TechnologyItem/TechnologyItem.jsx
@@ -3,15 +3,22 @@ import styles from "./TechnologyItem.module.scss";
 
 const TechnologyItem = ({ technology, type }) => {
 	const imageClasses = `${styles.Image} ${styles[type]}`;
-
+	const NavLinkClasses = ({ isActive }) =>
+		isActive ? `${styles.Link} ${styles.ActiveLink}` : `${styles.Link}`;
 	return (
 		<div className={styles.Technology}>
 			<div className={imageClasses}></div>
 			<div className={styles.Description}>
 				<nav className={styles.NavLinks}>
-					<NavLink to="/technology/launchvehicle">1</NavLink>
-					<NavLink to="/technology/spaceport">2</NavLink>
-					<NavLink to="/technology/spacecapsule">3</NavLink>
+					<NavLink to="/technology/launchvehicle" className={NavLinkClasses}>
+						1
+					</NavLink>
+					<NavLink to="/technology/spaceport" className={NavLinkClasses}>
+						2
+					</NavLink>
+					<NavLink to="/technology/spacecapsule" className={NavLinkClasses}>
+						3
+					</NavLink>
 				</nav>
 				<div className={styles.TextDescription}>
 					<h2>THE TERMINOLOGY ...</h2>
