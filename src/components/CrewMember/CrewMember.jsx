86880004b4ea802889d163d73af9d60bcf8eef7b
Isaac Novaes <isaac.novaes@hotmail.com>
2022-04-14 15:50:44 +0200

    Add desktop hover effects

--- src/components/CrewMember/CrewMember.jsx
+++ src/components/CrewMember/CrewMember.jsx
@@ -1,24 +1,53 @@
 import styles from "./CrewMember.module.scss";
 import { NavLink } from "react-router-dom";
+import { motion, useAnimation } from "framer-motion";
 
 const CrewMember = ({ crewMember }) => {
+	const imageControls = useAnimation();
+
+	const animationHandler = () => {
+		imageControls.start({
+			scale: [0.95, 1.05, 1],
+			opacity: [0, 0.8, 1],
+			transition: { duration: 0.5 },
+		});
+	};
+
 	const NavLinkClasses = ({ isActive }) =>
 		isActive ? `${styles.Link} ${styles.ActiveLink}` : `${styles.Link}`;
+
 	return (
 		<div className={styles.Member}>
-			<img
+			<motion.img
 				src={`../../../assets/crew/image-${crewMember.name
 					.replace(" ", "-")
 					.toLowerCase()}.webp`}
 				alt={`${crewMember.name} picture`}
+				animate={imageControls}
 			/>
 			<div className={styles.Divider}></div>
 			<div className={styles.Description}>
 				<nav className={styles.NavLinks}>
-					<NavLink to="/crew/douglashurley" className={NavLinkClasses} />
-					<NavLink to="/crew/markshuttleworth" className={NavLinkClasses} />
-					<NavLink to="/crew/victorglover" className={NavLinkClasses} />
-					<NavLink to="/crew/anoushehansari" className={NavLinkClasses} />
+					<NavLink
+						to="/crew/douglashurley"
+						className={NavLinkClasses}
+						onClick={animationHandler}
+					/>
+					<NavLink
+						to="/crew/markshuttleworth"
+						className={NavLinkClasses}
+						onClick={animationHandler}
+					/>
+					<NavLink
+						to="/crew/victorglover"
+						className={NavLinkClasses}
+						onClick={animationHandler}
+					/>
+					<NavLink
+						to="/crew/anoushehansari"
+						className={NavLinkClasses}
+						onClick={animationHandler}
+					/>
 				</nav>
 				<h2>{crewMember.role}</h2>
 				<h1>{crewMember.name}</h1>
